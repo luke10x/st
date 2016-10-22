e1663b514fa0f562920365b628148189059cc76c
Lukas Normantas <baciuvys@gmail.com>
2016-10-23 00:56:02 +0200

    Ubuntu mono as font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Inconsolata-dz for Powerline:style=dz:pixelsize=14:antialias=true:autohint=true";
+static char font[] = "Ubuntu Mono:style=dz:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
